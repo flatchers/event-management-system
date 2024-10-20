6ad574dff7174e4acc3342b194f9d87931690c0c
Heorhii Nadiradze <swain.volt@gmail.com>
2024-10-20 16:39:59 +0300

    added completed Event model

--- management/models.py
+++ management/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    class CategoryEvent(models.TextChoices):
+        CONFERENCE = "Conference"
+        WEBINAR = "Webinar"
+        WORKSHOP = "Workshop"
+        TRADE_SHOWS = "Trade shows"
+        JOB_FAIRS = "Job fairs"
+        NETWORKING = "Networking"
+        MUSIC_FESTIVAL = "Music festival"
+
+    event_category = models.CharField(max_length=255, choices=CategoryEvent)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    hold_date = models.DateTimeField()
+    seats = models.PositiveIntegerField
+    price = models.DecimalField(max_digits=10, decimal_places=2)
