0b1b9499e47dec48e814d42939aedb6489ae42cf
Heorhii Nadiradze <swain.volt@gmail.com>
2024-10-31 11:09:58 +0200

    fixed and changed

--- management/models.py
+++ management/models.py
@@ -17,15 +17,21 @@ class Event(models.Model):
 
     event_category = models.CharField(max_length=255, choices=CategoryEvent.choices)
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    hold_date = models.DateTimeField()
-    seats = models.PositiveIntegerField
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    hold_date = models.DateTimeField(blank=True, null=True)
+    seats = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class EventRegistration(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="registrations", on_delete=models.CASCADE)
     event = models.ForeignKey(Event, related_name="registrations", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.event.name
 
 
 class Payment:
